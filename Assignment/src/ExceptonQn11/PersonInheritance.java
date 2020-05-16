e95f23899f08409b03f63a2989e9a22a54aa5ed8
suchita-shakya <60432590+suchita-shakya@users.noreply.github.com>
2020-05-16 16:51:27 +0545

    Updated PersonInheritance

--- Assignment/src/ExceptonQn11/PersonInheritance.java
+++ Assignment/src/ExceptonQn11/PersonInheritance.java
@@ -8,15 +8,15 @@ class Person {
     public Person() {
     }
 
-    public void PersonInfo(String name){
+    public void personInfo(String name){
          this.name = name;
         System.out.println("The name of the person is "+name);
     }
 }
 class Politician extends Person{
 
-   public void Salary(double salary) {
-	   System.out.println("The salary of the"+name+" is "+salary);
+   public void salary(double salary) {
+	   System.out.println("The salary of the "+name+" is "+salary);
    }
 	
 	/*
@@ -28,8 +28,8 @@ class Politician extends Person{
     
 }
 class Sportsman extends Person{
-	public void Salary(double salary,double tax) {
-		   System.out.println("The salary of the"+name+" is "+(salary-tax));
+	public void salary(double salary,double tax) {
+		   System.out.println("The salary of the "+name+" is "+(salary-tax));
 	   }
 	
 	/*
@@ -45,12 +45,12 @@ class Sportsman extends Person{
 class PersonInheritance {
     public static void main(String[] args) {
     	  Politician politician = new Politician();
-    	  politician.PersonInfo("Hari");
-          politician.Salary(3000);
+    	  politician.personInfo("Hari");
+          politician.salary(3000);
 
           Sportsman sportman= new Sportsman();
-          sportman.PersonInfo("Sita");
-          sportman.Salary(4556,900);
+          sportman.personInfo("Sita");
+          sportman.salary(4556,900);
 
     }
 }
